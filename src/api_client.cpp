09a761ffdbc6f9975020b54f82d72ee4222bbc38
Markus Törnqvist <mjt@fadconsulting.com>
2014-11-23 12:29:52 +0200

    The commit with url construction

--- src/api_client.cpp
+++ src/api_client.cpp
@@ -1,4 +1,7 @@
 // vim: ts=4 sw=4 fenc=utf-8
+#include <QDebug>
+#include <QString>
+#include <QUrl>
 #include "api_client.h"
 
 using namespace BtxSecurity;
@@ -7,11 +10,22 @@ using namespace BtxSecurity;
  * CLIENT
  */
 
-ApiClient::ApiClient(QObject *parent) : QObject(parent) {
-    qDebug("construct");
+ApiClient::ApiClient(QString baseUrl, QObject *parent) : QObject(parent) {
+    if (!baseUrl.endsWith("/")) baseUrl += "/";
+
+    this->baseUrl = baseUrl;
+}
+
+QUrl ApiClient::genPath(QString path) {
+    if (!path.startsWith("/")) path = "/" + path;
+
+    QUrl url = baseUrl + QString("v") + QString::number(ApiClient::apiVersion) + path;
+    qDebug() << url.toString() << "\n";
+
+    return url;
 }
 
 ApiClient::~ApiClient() {
-    qDebug("destruct");
+    qDebug("bye");
 }
 
--- src/api_client.h
+++ src/api_client.h
@@ -1,13 +1,21 @@
 // vim: ts=4 sw=4 fenc=utf-8
 #include <QObject>
+#include <QUrl>
 
 namespace BtxSecurity {
     class ApiClient : public QObject {
         Q_OBJECT;
         public:
-            explicit ApiClient(QObject *parent = 0);
+            explicit ApiClient(QString baseUrl, QObject *parent = 0);
 
             ~ApiClient();
+        protected:
+            // members
+            QString baseUrl;
+            static const int apiVersion = 1;
+
+            // methods
+            QUrl genPath(QString);
     };
 }
 
