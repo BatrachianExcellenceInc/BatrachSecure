db199b28d352069f43c2fe57cded148d37d096b4
Markus Törnqvist <mjt@fadconsulting.com>
2015-05-03 18:55:54 +0300

    Do some work for the ClientConf

--- src/client_conf.cpp
+++ src/client_conf.cpp
@@ -23,17 +23,39 @@ ClientConfException::ClientConfException(QString msg) : BtxException(msg){
  * CLIENT CONF
  */
 
-ClientConf::ClientConf(QString confDir, QObject *parent) : QObject(parent) {
+ClientConf::ClientConf(QString baseUrl, QString confDir, QString number, QObject *parent) : QObject(parent) {
+    // Some basics are always required
+    if (!baseUrl.endsWith("/")) baseUrl += "/";
+    this->baseUrl = baseUrl;
+
     // XXX: Will not work with other separator
     this->confDirPath = QDir::homePath() + "/" + confDir;
     this->confDir = QDir(this->confDirPath);
 
+    qDebug() << QString("ClientConf constructed with number " + number);
+    this->number = number;
+
     this->verifyConfDir();
+    this->verifyNumber();
+    this->verifyPassword();
     this->verifyRegistrationId();
     this->verifySignalingKey();
     this->verifyIdentityKeyPair();
 
     this->preKeyStore = BtxPreKeyStore(this->confDirPath + QString("/prekeys"), this->confDirPath + QString("/signed_prekeys"), this->keyHelper);
+}
+
+QString ClientConf::getBaseUrl() {
+    return this->baseUrl;
+}
+
+QString ClientConf::getNumber() {
+    qDebug() << QString("getNumber() returning ") + this->number;
+    return this->number;
+}
+
+QString ClientConf::getPassword() {
+    return this->password;
 }
 
 quint64 ClientConf::getRegistrationId() {
@@ -50,6 +72,43 @@ void ClientConf::verifyConfDir() {
         if (!this->confDir.mkpath(this->confDirPath)) {
             throw ClientConfException(QString("Could not create conf dir " + this->confDirPath));
         }
+    }
+}
+
+void ClientConf::verifyNumber() {
+    QFile numberFile(this->confDirPath + QString("/number.conf"));
+    if (!numberFile.exists()) {
+        if (!numberFile.open(QIODevice::WriteOnly)) {
+            throw ClientConfException(QString("Could not create phone number file " + numberFile.fileName()));
+        }
+
+        QDataStream numberFileDS(&numberFile);
+        numberFile.open(QFile::ReadOnly);
+        numberFileDS << this->number;
+        numberFile.close();
+    }
+}
+
+void ClientConf::verifyPassword() {
+    QFile passwordFile(this->confDirPath + QString("/password.conf"));
+    if (!passwordFile.exists()) {
+        if (!passwordFile.open(QIODevice::WriteOnly)) {
+            throw ClientConfException(QString("Could not create random password file " + passwordFile.fileName()));
+        }
+
+        QByteArray pwChars = this->keyHelper.getRandomBytes(16);
+        QString pw(pwChars);
+
+        QDataStream pwWriter(&passwordFile);
+        pwWriter << pw;
+        passwordFile.close();
+
+        this->password = pw;
+    } else {
+        QDataStream pwReader(&passwordFile);
+        passwordFile.open(QFile::ReadOnly);
+        pwReader >> this->password;
+        passwordFile.close();
     }
 }
 
